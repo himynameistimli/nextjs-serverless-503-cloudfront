4d2f20c6b1c4c1f14ba5f7fa09f4155842d59db7
Timothy Li <himynameistimli@gmail.com>
2020-10-21 16:43:01 +0800

    Update serverless-next

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,16 +6,13 @@ export default function Home() {
 
       <div style={{padding: "30px", display: "flex", flexDirection: "column", fontFamily: "sans-serif" }}>
         <h1>{`Cloudfront 503/404 Error`}</h1>
-        <p>
-          Both the SSR pages will return a 503/404 error when you refresh the page. They function normally when navigating from the index. Locally works fine on refresh.
-        </p>
 
         <p>
-          *On <red>Type</red> and <green>Slug</green> Pages, I have set <strong>fallback: false</strong>, but if I set it to <strong>true</strong>, and refresh a Test page, it will try to load the <red>Type</red> pages instead
+          Dynamic SSR pages will return a 503/404 error when you refresh the page. They function normally when navigating from the index
         </p>
 
-        <Link href="/test">{`SSR Test (SSR)`}</Link>
-        <Link href="/test/nested">{`SSR Nested Test (SSR)`}</Link>
+        <Link href="/test">{`SSR Static Test`}</Link>
+        <Link href="/test/nested">{`SSR Dynamic Test`}</Link>
         <br/>
         <Link href="/cat">{`Type Cat`}</Link>
         <Link href="/dog">{`Type Dog`}</Link>
